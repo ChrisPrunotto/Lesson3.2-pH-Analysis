10105b36a2c570bcc3445bc81c1556089ecc1e27
Chris Prunotto <Chris.Prunotto@gmail.com>
2020-08-06 21:53:14 -0500

    Update Lesson3.2-pH Analysis.cpp
    Simple pH analysis to demonstrate if-elseif-else relationships

--- Lesson3.2-pH Analysis/Lesson3.2-pH Analysis.cpp
+++ Lesson3.2-pH Analysis/Lesson3.2-pH Analysis.cpp
@@ -5,16 +5,22 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double ph;
+	std::cout << "Enter the pH value: ";
+	std::cin >> ph;
+
+	// Write the if, else if, else here:
+	if (ph > 7)
+	{
+		std::cout << "Basic\n";
+	}
+	else if (ph < 7)
+	{
+		std::cout << "Acidic\n";
+	}
+	else
+	{
+		std::cout << "Neutral\n";
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
